a1fb2dc06cd446b0abae67ef72a624d758b7d082
TechQuery <shiy007@qq.com>
2017-10-08 02:35:41 +0800

    【重构】独立出 Progress、Tab 组件，以简化 Repository 相关页面的代码

--- page/Repository/MileStone/index.js
+++ page/Repository/MileStone/index.js
@@ -1,29 +1,16 @@
 require(['jquery', 'EasyWebApp'],  function ($, EWA) {
 
-    function set_Percent() {
+    var iWebApp = new EWA();
 
-        this.percent = (
-            this.closed_issues / (this.open_issues + this.closed_issues)
-        ) * 100;
-    }
 
-    EWA.component(function (_data_) {
+    EWA.component(function () {
 
-        if ( _data_.list )
-            $.each(_data_.list, set_Percent);
-        else
-            set_Percent.call( _data_.milestone );
+        this.on('ready',  function () {
 
-        _data_.barState = function () {
+            $.each(this.$_View.find(':listview').view(),  function () {
 
-            var due = new Date( this.due_on ),  state = 'progress-bar-striped ';
-
-            if (Date.now() > due)
-                return  state + 'progress-bar-danger';
-
-            return  (this.percent < 100)  ?
-                (state + 'active')  :
-                'progress-bar-success';
-        };
+                iWebApp.load( this.childOf()[0] );
+            });
+        });
     });
 });
